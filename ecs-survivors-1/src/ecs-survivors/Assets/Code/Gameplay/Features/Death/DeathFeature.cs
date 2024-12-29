3c04d2898d998027c45d0dcc0a687b5c43060a0c
Kenan Ibragimov <102488775+SHIPZz@users.noreply.github.com>
2024-12-29 03:14:39 +0300

    init

---
+++ ecs-survivors-1/src/ecs-survivors/Assets/Code/Gameplay/Features/Death/DeathFeature.cs
@@ -0,0 +1,31 @@
+﻿using Entitas;
+
+namespace Code.Gameplay.Features.Death
+{
+    public class DeathFeature : Feature
+    {
+        public DeathFeature()
+        {
+            
+        }
+    }
+
+    public class DeathSystem : IExecuteSystem
+    {
+        private readonly IGroup<GameEntity> _entities;
+
+        public DeathSystem(GameContext game)
+        {
+            // _entities = game.GetGroup(GameMatcher
+                // .AllOf(GameMatcher.matcher));
+        }
+
+        public void Execute()
+        {
+            foreach (GameEntity entity in _entities)
+            {
+                
+            }
+        }
+    }
+}

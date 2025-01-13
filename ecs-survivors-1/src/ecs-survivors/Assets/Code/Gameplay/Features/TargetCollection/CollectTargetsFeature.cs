9071f4b9a3ef7f9e6d89a71e4b7a5828456e24cc
Kenan Ibragimov <102488775+SHIPZz@users.noreply.github.com>
2025-01-13 06:50:12 +0300

    fix usings&namespaces

--- ecs-survivors-1/src/ecs-survivors/Assets/Code/Gameplay/Features/TargetCollection/CollectTargetsFeature.cs
+++ ecs-survivors-1/src/ecs-survivors/Assets/Code/Gameplay/Features/TargetCollection/CollectTargetsFeature.cs
@@ -1,4 +1,5 @@
 ﻿using Code.Gameplay.Features.Movement;
+using Code.Gameplay.Features.Movement.Factory;
 using Code.Gameplay.Features.TargetCollection.Systems;
 
 namespace Code.Gameplay.Features.TargetCollection
